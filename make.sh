0df5d7fc2748f5c7989ec1e1b41d2e5f1ae858b1
Srirama m2012 <bsrirama@gmail.com>
2019-05-06 17:40:16 +0700

    all git in travis , git push with key

--- make.sh
+++ make.sh
@@ -8,11 +8,14 @@ cd /home/travis/build/sriramab/ArcGISAddinsTemp
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
 python /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate/makeaddin.py
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
+git remote rm origin
+git remote add origin https://sriramab:$sriramab_KEY@github.com/sriramab/ArcGISAddins.git
+
 git remote show origin
 git add -f /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate/Aggregate.esriaddin
 git status
 git commit -m "created addin"
-git push
+git push origin HEAD:master
 #git remote add origin https://sriramab:$sriramab_KEY@github.com/sriramab/ArcGISAddins.git
 
 }
