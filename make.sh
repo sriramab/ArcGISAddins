8d31e191e03a47565a4fa3e3fa727550b97ec03e
Srirama m2012 <bsrirama@gmail.com>
2019-05-06 15:04:31 +0700

    all git in travis and shell3

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 
 
 build_addin (){
-git clone --depth=50 --branch=master https://github.com/sriramab/ArcGISAddins_release.git 
+git clone --depth=50 --branch=master https://github.com/sriramab/ArcGISAddins.git 
 cd /home/travis/build/sriramab/ArcGISAddins
 ls ./Aggregate
 python ./Aggregate/makeaddin.py
