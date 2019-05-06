0436b80e4bf06329d45580d4ef89557cf54f97e2
Srirama m2012 <bsrirama@gmail.com>
2019-05-06 18:23:19 +0700

    all git in travis , commit removal of previous addin

--- make.sh
+++ make.sh
@@ -6,6 +6,7 @@ build_addin (){
 git clone --depth=50 --branch=master https://github.com/sriramab/ArcGISAddins.git /home/travis/build/sriramab/ArcGISAddinsTemp
 cd /home/travis/build/sriramab/ArcGISAddinsTemp
 rm ./Aggregate/Aggregate.esriaddin
+git commit -m "remove previous addin"
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
 python /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate/makeaddin.py
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
