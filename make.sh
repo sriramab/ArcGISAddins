d5fc600fb35028a035bc8063e80b132bfcb57eb7
Srirama m2012 <bsrirama@gmail.com>
2019-05-06 16:56:08 +0700

    all git in travis and separate folder

--- make.sh
+++ make.sh
@@ -5,8 +5,8 @@
 build_addin (){
 git clone --depth=50 --branch=master https://github.com/sriramab/ArcGISAddins.git /home/travis/build/sriramab/ArcGISAddinsTemp
 cd /home/travis/build/sriramab/ArcGISAddinsTemp
-ls ./Aggregate
-python ./Aggregate/makeaddin.py
+ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
+python /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate/makeaddin.py
 git status
 #git remote add origin https://sriramab:$sriramab_KEY@github.com/sriramab/ArcGISAddins.git
 
