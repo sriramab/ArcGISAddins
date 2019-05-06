4c8f93a21c58cdb834b95f0a3a5797ae76e66cf8
Srirama m2012 <bsrirama@gmail.com>
2019-05-06 17:03:57 +0700

    all git in travis , get origin

--- make.sh
+++ make.sh
@@ -8,6 +8,7 @@ cd /home/travis/build/sriramab/ArcGISAddinsTemp
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
 python /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate/makeaddin.py
 ls /home/travis/build/sriramab/ArcGISAddinsTemp/Aggregate
+git remote show origin
 git status
 #git remote add origin https://sriramab:$sriramab_KEY@github.com/sriramab/ArcGISAddins.git
 
